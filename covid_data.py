5070f465b70e5609358cc6a6a15afe7699efc25c
Enrico Lumetti <enrico.lumetti@gmail.com>
2020-03-10 01:33:52 +0100

    Add more plot variables and cumulative data for whole Italy

--- covid_data.py
+++ covid_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from glob import glob
 import os
 
@@ -7,21 +8,26 @@ REGIONI_DATA_PATH = 'COVID-19/dati-regioni/dpc-covid19-ita-regioni.csv'
 df = pd.read_csv(REGIONI_DATA_PATH, parse_dates=['data'])
 
 fields = {
-    'totale_attualmente_positivi': 'Totale attualmente positivi',
-    'totale_ospedalizzati': 'Pazienti ospedalizzati',
+    'totale_casi': 'Casi totali',
+    'totale_attualmente_positivi': 'Attualmente positivi',
+    'isolamento_domiciliare': 'Isolamento domiciliare',
+    'totale_ospedalizzati': 'Positivi ospedalizzati',
     'terapia_intensiva': 'Pazienti in terapia intensiva',
     'deceduti': 'Pazienti deceduti',
     'dimessi_guariti': 'Pazienti dimessi',
+    'tamponi': 'Numero di tamponi',
 }
 
 regions = df['denominazione_regione'].unique()
+regions = np.insert(regions, 0, 'Italia')
 
 def get_regional_covid_data():
     global df
     dff = df.pivot(index='data',columns='denominazione_regione', values=fields.keys())
-
-    return dff
+    sum_df = dff.sum(axis=1, level=0)
+    sum_df.columns = pd.MultiIndex.from_product([sum_df.columns, ['Italia']])
+    return pd.concat([dff,sum_df], axis=1).sort_index(axis=1)
 
 if __name__ == '__main__':
     df = get_regional_covid_data()
-    print(df['totale_attualmente_positivi']['Abruzzo'])
+    print(df.index)
