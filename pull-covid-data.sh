0b76ce4a5fe44f73dcf9b5155b732c14dcaae353
Enrico Lumetti <enrico.lumetti@gmail.com>
2020-05-10 12:48:48 +0200

    pull with --rebse option

--- pull-covid-data.sh
+++ pull-covid-data.sh
@@ -4,6 +4,6 @@ if [ ! -d "COVID-19" ]; then
     git clone https://github.com/pcm-dpc/COVID-19
 fi
 
-cd COVID-19 && git pull origin master
+cd COVID-19 && git pull --rebase origin master
 cd ..
 date -Is > update_timestamp.txt
