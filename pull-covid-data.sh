c80b6ef495c3333334503b7b10f147eeafced1a1
Enrico Lumetti <enrico.lumetti@gmail.com>
2020-10-27 18:53:38 +0100

    make pull-covid-data.sh more error proof

--- pull-covid-data.sh
+++ pull-covid-data.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+repo_url=https://github.com/pcm-dpc/COVID-19
+
 if [ ! -d "COVID-19" ]; then
-    git clone https://github.com/pcm-dpc/COVID-19
+    git clone $repo_url
 fi
 
 cd COVID-19 && git pull --rebase origin master
+res=$?
 cd ..
+if [ 0 -ne $res ]; then
+   rm -rf COVID-19
+   git clone $repo_url
+fi
 date -Is > update_timestamp.txt
